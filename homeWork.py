fc6e3e183bba3a7b3df9ed03a0aea57f96c55947
Matvey <m.vyaznin@gmail.coma>
2023-10-05 19:05:12 +0500

    Изменил

--- homeWork.py
+++ homeWork.py
@@ -1,88 +1,160 @@
-# Показывает информацию в файле
-def show_data(filename):
-print("\nПП | ФИО | Телефон")
-with open(filename, r, encoding-=utf-8″) as data:
-print(data.read())
-print("")
+import os, re
 
-# Записывает информацию в файл
-def export_data(filename):
-with open(filename, «r», encoding=»utf-8″) as data:
-tel_file = data.read()
-num = len(tel_file.split("\n"))
-with open(filename, «a», encoding=»utf-8″) as data:
-fio = input(«Введите ФИО: «)
-phone_number = input(«Введите номер телефона: «)
-data.write(f»{num} | {fio} | {phone_number}"\n")
-print(f»Добавлена запись : {num} | {fio} | {phone_number}"\n")
 
-# Изменяет информацию из файла
-def edit_data(filename):
-print("\nПП | ФИО | Телефон")
-with open(filename, «r», encoding=’utf-8′) as data:
-tel_book = data.read()
-print(tel_book)
-print(«»)
-index_delete_data = int(input(«Введите номер строки для редактирования: «)) — 1
-tel_book_lines = tel_book.split("\n")
-edit_tel_book_lines = tel_book_lines[index_delete_data]
-elements = edit_tel_book_lines.split(» | «)
-fio = input(«Введите ФИО: «)
-phone = input(«Введите номер телефона: «)
-num = elements[0]
-if len(fio) == 0:
-fio = elements[1]
-if len(phone) == 0:
-phone = elements[2]
-edited_line = f»{num} | {fio} | {phone}»
-tel_book_lines[index_delete_data] = edited_line
-print(f»Запись — {edit_tel_book_lines}, изменена на — {edited_line}"\n")
-with open(filename, «w», encoding=’utf-8′) as f:
-f.write("\n».join(tel_book_lines))
+def phone_format(n):  # форматирование телефонного номера
+    n = n.removeprefix("+")
+    n = re.sub("[ ()-]", "", n)
+    return format(int(n[:-1]), ",").replace(",", "-") + n[-1]
 
-# Удаляет информацию из файла
-def delete_data(filename):
-print("\nПП | ФИО | Телефон")
-with open(filename, «r», encoding=»utf-8″) as data:
-tel_book = data.read()
-print(tel_book)
-print(«»)
-index_delete_data = int(input(«Введите номер строки для удаления: «)) — 1
-tel_book_lines = tel_book.split("\n")
-del_tel_book_lines = tel_book_lines[index_delete_data]
-tel_book_lines.pop(index_delete_data)
-print(f»Удалена запись: {del_tel_book_lines}"\n")
-with open(filename, «w», encoding=’utf-8′) as data:
-data.write("\n».join(tel_book_lines))
 
-def main():
-my_choice = -1
-file_tel = «tel.txt»
+def printData(data):  # Функция вывода телефонной книги в консоль
+    phoneBook = []
+    splitLine = "=" * 49
+    print(splitLine)
+    print(" №  Lastname        Name          Phone Numbers")
+    print(splitLine)
+    personID = 1
 
-# Создает файл если его нет в папке
-with open(file_tel, «a», encoding=»utf-8″) as file:
-file.write(«»)
+    for contact in data:
+        lastName, name, phone = contact.rstrip().split(",")
+        phoneBook.append(
+            {
+                "ID": personID,
+                "lastName": lastName,
+                "name": name,
+                "phone": phone_format(phone),
+            }
+        )
+        personID += 1
 
-while my_choice != 0:
-print(«Выберите одно из действий:»)
-print(«1 — Вывести инфо на экран»)
-print(«2 — Произвести экпорт данных»)
-print(«3 — Произвести изменение данных»)
-print(«4 — Произвести удаление данных»)
-print(«0 — Выход из программы»)
-action = int(input(«Действие: «))
-if action == 1:
-show_data(file_tel)
-elif action == 2:
-export_data(file_tel)
-elif action == 3:
-edit_data(file_tel)
-elif action == 4:
-delete_data(file_tel)
-else:
-my_choice = 0
+    for contact in phoneBook:
+        personID, lastName, name, phone = contact.values()
+        print(f"{personID:>2}. {lastName:<15} {name:<10} -- {phone:<15}")
 
-print(«До свидания»)
+    print(splitLine)
 
-if __name__ == «__main__»:
-main()
+
+def showContacts(fileName):  # Функция открытия телефонной книги
+    os.system("cls")
+    phoneBook = []
+    with open(fileName, "r", encoding="UTF-8") as file:
+        data = sorted(file.readlines())
+        printData(data)
+    input("\n--- press any key ---")
+
+
+def addContact(fileName):  # Функция добавления нового контакта в телефонную книгу
+    os.system("cls")
+    with open(fileName, "a", encoding="UTF-8") as file:
+        res = ""
+        res += input("Input a Surname of Contact: ") + ","
+        res += input("Input a Name of Contact: ") + ","
+        res += input("Input a Phone Number of Contact: ")
+
+        file.write(res + "\n")
+
+    input("\nContact was successfully added!\n--- press any key ---")
+
+
+def findContact(fileName):  # Функция поиска контактов в телефонной книге
+    os.system("cls")
+    target = input("Input Item of Contact for searching: ")
+    result = []
+    with open(fileName, "r", encoding="UTF-8") as file:
+        data = file.readlines()
+        for person in data:
+            if target in person:
+                result.append(person)
+                # break
+
+    if len(result) != 0:
+        printData(result)
+    else:
+        print(f"There is no Contact with this Item '{target}'.")
+
+    input("--- press any key ---")
+
+
+def changeContact(fileName):  # Функция изменения информации в контакте
+    os.system("cls")
+    phoneBook = []
+    with open(fileName, "r", encoding="UTF-8") as file:
+        data = sorted(file.readlines())
+        printData(data)
+
+        numberContact = int(
+            input("Input Number of Contact for changing or 0 for return Main Menu: ")
+        )
+        print(data[numberContact - 1].rstrip().split(","))
+        if numberContact != 0:
+            newLastName = input("Input new Lastname: ")
+            newName = input("Input new Name: ")
+            newPhone = input("Input new Phone: ")
+            data[numberContact - 1] = (
+                newLastName + "," + newName + "," + newPhone + "\n"
+            )
+            with open(fileName, "w", encoding="UTF-8") as file:
+                file.write("".join(data))
+                print("\nContact was successfully changed!")
+                input("\n--- press any key ---")
+        else:
+            return
+
+
+def deleteContact(fileName):  # Функция удаления контакта из телефонной книги
+    os.system("cls")
+    with open(fileName, "r+", encoding="UTF-8") as file:
+        data = sorted(file.readlines())
+        printData(data)
+
+        numberContact = int(
+            input("Input Number of Contact for deleting or 0 for return Main Menu: ")
+        )
+        if numberContact != 0:
+            print(f"Deleting record: {data[numberContact-1].rstrip().split(',')}\n")
+            data.pop(numberContact - 1)
+            with open(fileName, "w", encoding="UTF-8") as file:
+                file.write("".join(data))
+
+        else:
+            return
+
+    input("--- press any key ---")
+
+
+def drawInterface():  # Функция рисования интерфейса главного меню
+    print("#####   PHONE BOOK   #####")
+    print("=" * 26)
+    print(" [1] -- Show Contacts")
+    print(" [2] -- Add Contacts")
+    print(" [3] -- Find Contacts")
+    print(" [4] -- Change Contacts")
+    print(" [5] -- Delete Contacts")
+    print("\n [0] -- Exit")
+    print("=" * 26)
+
+
+def main(file_name):  # Функция реализации главного меню
+    while True:
+        os.system("cls")
+        drawInterface()
+        userChoice = int(input("Input a Number for 1 to 5 or 0 for Exit: "))
+
+        if userChoice == 1:
+            showContacts(file_name)
+        elif userChoice == 2:
+            addContact(file_name)
+        elif userChoice == 3:
+            findContact(file_name)
+        elif userChoice == 4:
+            changeContact(file_name)
+        elif userChoice == 5:
+            deleteContact(file_name)
+        elif userChoice == 0:
+            print("Thank you!")
+            return
+
+
+path = "phonebook.txt"
+
+main(path)
